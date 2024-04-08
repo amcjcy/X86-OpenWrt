93dafe0692676dc23e087f4c15a0eb5d18f9f1d1
amcjcy <82363754+amcjcy@users.noreply.github.com>
2024-04-08 20:38:05 +0800

    Update diy-part1.sh

--- X86/diy-part1.sh
+++ X86/diy-part1.sh
@@ -17,6 +17,5 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
-echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
 git clone https://github.com/bootli/luci-app-v2ray-server.git package/v2ray
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
